7bf1dbc0d07a11647eab949eb555bd2319daf0d4
Mukesh buldak <mukeshkumar.buldak12@gmail.com>
2022-01-21 09:25:42 +0530

    server initialization and setup

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("Connected to MONGODB database.");
+}).catch((err) => {
+  console.log("Error occured in connection to db -" + err);
+});
+
+app.get("/hello-world", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Backend server has started at " + process.env.PORT);
+});
